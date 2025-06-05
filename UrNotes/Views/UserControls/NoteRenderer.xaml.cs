d31cdffd5744abb28297c2dd254553fa4741911f
Raúl Villarreal <raulevillar30@gmail.com>
2025-06-04 21:14:20 -0500

    (Addition) CustomRichTextBox + (Future)NoteRenderer: A richtextbox control with zoom capabilites was added to simulate input for future notes. It is nested inside a NoteRenderer control, which, is essentially a wrapper that will interact with NotesViewModel from MainView in order send the correct .rtf info to the CustomRichTextBox

---
+++ UrNotes/Views/UserControls/NoteRenderer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace UrNotes.Views.UserControls {
+  public partial class NoteRenderer : UserControl {
+
+    public NoteRenderer() {
+      InitializeComponent();
+    }
+
+  }
+}
